0f180575cbb7c6e38bf9fb68cddb4e51cb264739
blake <wubo2560@163.com>
2019-08-05 10:37:22 +0800

    Initial commit

---
+++ src/main/java/com/itbounds/dev/gitdrill/GitDrillApplication.java
@@ -0,0 +1,13 @@
+package com.itbounds.dev.gitdrill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDrillApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GitDrillApplication.class, args);
+  }
+
+}
